5cdd7bb248d80aaf9a2dd83e7ac47d1a9c45e752
TrRaviK <ravi.kumar@mediaagility.com>
2022-01-19 10:13:11 +0530

    base project

---
+++ src/test/java/com/practice/unittestingpractice/UnitTestingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.unittestingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
